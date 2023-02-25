43e813eb2d69603d943c8d5afc13f80db035b70b
flydecisive <flydecisive@yandex.ru>
2023-02-25 13:02:43 +0300

    ending project

--- expanding_cards/script.js
+++ expanding_cards/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var items = document.querySelectorAll('.container__item');
+    var titles = document.querySelectorAll('.title');
     items.forEach(function (item) {
         item.addEventListener('click', function () {
             if (!(item.classList.contains('active'))) {
@@ -7,8 +8,12 @@ document.addEventListener('DOMContentLoaded', function () {
                     item.classList.remove('active');
                 });
                 item.classList.add('active');
+                titles.forEach(function (title) {
+                    title.classList.add('hidden');
+                });
+                var title = item.querySelector('.title');
+                title === null || title === void 0 ? void 0 : title.classList.remove('hidden');
             }
-            ;
         });
     });
 });
--- expanding_cards/script.ts
+++ expanding_cards/script.ts
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const items = document.querySelectorAll('.container__item');
+    const titles = document.querySelectorAll('.title');
     items.forEach((item) => {
         item.addEventListener('click', () => {
             if (!(item.classList.contains('active'))) {
@@ -7,7 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
                     item.classList.remove('active');
                 });
                 item.classList.add('active');
-            };
+                titles.forEach((title) => {
+                    title.classList.add('hidden');
+                });
+                const title = item.querySelector('.title');
+                title?.classList.remove('hidden');
+            }
         });
     });
 });
