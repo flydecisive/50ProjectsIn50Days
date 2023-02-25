7659c52ec19b0dd4345f2ff5fe3d982d7e1c4e8d
flydecisive <flydecisive@yandex.ru>
2023-02-25 17:20:23 +0300

    change solution

--- expanding_cards/script.js
+++ expanding_cards/script.js
@@ -1,19 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
     var items = document.querySelectorAll('.container__item');
-    var titles = document.querySelectorAll('.title');
+    // const titles = document.querySelectorAll('.title');
     items.forEach(function (item) {
         item.addEventListener('click', function () {
-            if (!(item.classList.contains('active'))) {
-                items.forEach(function (item) {
-                    item.classList.remove('active');
-                });
-                item.classList.add('active');
-                titles.forEach(function (title) {
-                    title.classList.add('hidden');
-                });
-                var title = item.querySelector('.title');
-                title === null || title === void 0 ? void 0 : title.classList.remove('hidden');
-            }
+            changeActive(item);
+            changeHidden(item);
+            // if (!(item.classList.contains('active'))) {
+            //     items.forEach((item) => {
+            //         item.classList.remove('active');
+            //     });
+            //     item.classList.add('active');
+            //     titles.forEach((title) => {
+            //         title.classList.add('hidden');
+            //     });
+            //     const title = item.querySelector('.title');
+            //     title?.classList.remove('hidden');
+            // }
         });
     });
 });
+function changeActive(item) {
+    var items = document.querySelectorAll('.container__item');
+    if (!(item.classList.contains('active'))) {
+        items.forEach(function (item) {
+            item.classList.remove('active');
+        });
+        item.classList.add('active');
+    }
+}
+function changeHidden(item) {
+    var titles = document.querySelectorAll('.title');
+    titles.forEach(function (title) {
+        title.classList.add('hidden');
+    });
+    var title = item.querySelector('.title');
+    title === null || title === void 0 ? void 0 : title.classList.remove('hidden');
+}
--- expanding_cards/script.ts
+++ expanding_cards/script.ts
@@ -1,19 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const items = document.querySelectorAll('.container__item');
-    const titles = document.querySelectorAll('.title');
     items.forEach((item) => {
         item.addEventListener('click', () => {
-            if (!(item.classList.contains('active'))) {
-                items.forEach((item) => {
-                    item.classList.remove('active');
-                });
-                item.classList.add('active');
-                titles.forEach((title) => {
-                    title.classList.add('hidden');
-                });
-                const title = item.querySelector('.title');
-                title?.classList.remove('hidden');
-            }
+            changeActive(item);
+            changeHidden(item);
         });
     });
 });
+
+function changeActive(item: Element): void {
+    const items = document.querySelectorAll('.container__item');
+    if (!(item.classList.contains('active'))) {
+        items.forEach((item) => {
+            item.classList.remove('active');
+        });
+        item.classList.add('active');
+    }
+}
+
+function changeHidden(item: Element): void {
+    const titles = document.querySelectorAll('.title');
+    titles.forEach((title) => {
+                title.classList.add('hidden');
+    });
+    const title = item.querySelector('.title');
+    title?.classList.remove('hidden');
+}
