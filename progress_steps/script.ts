811217622866af69a571140d74ab45d1bf2443e7
flydecisive <flydecisive@yandex.ru>
2023-02-26 12:56:40 +0300

    start progress steps

---
+++ progress_steps/script.ts
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.container');
+    const elementsContainer = container?.querySelector('.elements__container');
+    const buttonsContainer = container?.querySelector('buttons__container');
+    const elements = elementsContainer?.querySelectorAll('.elements');
+    const previousButton = buttonsContainer?.querySelector('.previous__button');
+    const nextButton = buttonsContainer?.querySelector('.next__button');
+    elements?.forEach((element) => {
+
+    });
+});
