69a978d52241d7d531675df31d693915c8ad0770
flydecisive <flydecisive@yandex.ru>
2023-02-26 12:17:07 +0300

    code optimizetion

--- expanding_cards/script.js
+++ expanding_cards/script.js
@@ -1,26 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     var items = document.querySelectorAll('.container__item');
-    // const titles = document.querySelectorAll('.title');
+    var titles = document.querySelectorAll('.title');
     items.forEach(function (item) {
         item.addEventListener('click', function () {
-            changeActive(item);
-            changeHidden(item);
-            // if (!(item.classList.contains('active'))) {
-            //     items.forEach((item) => {
-            //         item.classList.remove('active');
-            //     });
-            //     item.classList.add('active');
-            //     titles.forEach((title) => {
-            //         title.classList.add('hidden');
-            //     });
-            //     const title = item.querySelector('.title');
-            //     title?.classList.remove('hidden');
-            // }
+            var title = item.querySelector('.title');
+            changeActive(items, item);
+            changeHidden(titles, title);
         });
     });
 });
-function changeActive(item) {
-    var items = document.querySelectorAll('.container__item');
+function changeActive(items, item) {
     if (!(item.classList.contains('active'))) {
         items.forEach(function (item) {
             item.classList.remove('active');
@@ -28,11 +17,9 @@ function changeActive(item) {
         item.classList.add('active');
     }
 }
-function changeHidden(item) {
-    var titles = document.querySelectorAll('.title');
+function changeHidden(titles, title) {
     titles.forEach(function (title) {
         title.classList.add('hidden');
     });
-    var title = item.querySelector('.title');
     title === null || title === void 0 ? void 0 : title.classList.remove('hidden');
 }
--- expanding_cards/script.ts
+++ expanding_cards/script.ts
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const items = document.querySelectorAll('.container__item');
+    const titles = document.querySelectorAll('.title');
     items.forEach((item) => {
         item.addEventListener('click', () => {
-            changeActive(item);
-            changeHidden(item);
+            const title = item.querySelector('.title');
+            changeActive(items, item);
+            changeHidden(titles, title);
         });
     });
 });
 
-function changeActive(item: Element): void {
-    const items = document.querySelectorAll('.container__item');
+function changeActive(items: NodeListOf<Element>, item: Element): void {
     if (!(item.classList.contains('active'))) {
         items.forEach((item) => {
             item.classList.remove('active');
@@ -18,11 +19,9 @@ function changeActive(item: Element): void {
     }
 }
 
-function changeHidden(item: Element): void {
-    const titles = document.querySelectorAll('.title');
+function changeHidden(titles: NodeListOf<Element>, title: Element | null): void {   
     titles.forEach((title) => {
         title.classList.add('hidden');
     });
-    const title = item.querySelector('.title');
     title?.classList.remove('hidden');
 }
